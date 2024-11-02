d25787d0e7cdcae01fe7a7669eb201533d889000
Project Code Ltd <projectcodeltd@iren.local>
2024-11-02 00:58:56 -0700

    add inputs

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/screens/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:brew_crew/shared/constants.dart';
 
 class SignIn extends StatefulWidget {
   // const SignIn({super.key});
@@ -46,14 +47,16 @@ class _SignInState extends State<SignIn> {
             children: <Widget>[
               const SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
                 validator: (val) => val!.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
-                decoration: const InputDecoration(hintText: 'Email'),
+                // decoration: const InputDecoration(hintText: 'Email'),
               ),
               const SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
                 obscureText: true,
                 validator: (val) => val!.length < 6 ? 'Enter a password 6+ chars long' : null,
                 onChanged: (val) {
---
+++ lib/shared/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const textInputDecoration = InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.pink, width: 2.0),
+                  )
+                
+);
